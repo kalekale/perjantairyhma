1b1d57c1bbc39eea81eae3afac631331e5d9bdbc
kalekale <lehikoinen.kalle@gmail.com>
2016-04-14 11:32:05 +0300

    voi lisätä muitakin tietoja kuin title

--- server.js
+++ server.js
@@ -5,54 +5,28 @@ var session = require('express-session');
 var db = require('./db');
 var bodyParser = require('body-parser');
 
-
 var app = express();
-
 
 app.use(bodyParser.json());	
 
 app.use('/', express.static(path.join(__dirname)));
 
-//list all
 app.get('/getAll',function(req,res){
 	
 	db.references.findAsync()
 	.then(function(all) {
 		res.json(all);
-		alert(all);
-		res.send(200);
-	})
-  
+	})  
 });
 
 app.post('/new', function(req, res) {
-	//db pitää viitata johonkin kokoilmaan
 	db.references.insertAsync(
-		{ title: req.body.title}
+		req.body
 	)
  	.then(function() {
     res.sendStatus(200);
   });
 });
-
-/*app.post('/new', function(req, res) {
-
-	db.reference.Asyncupdate(
-		{ project: req.params.projectID},
-		{ $push:
-			{references: req.body.reference}
-		}
-	)
- 	.then(function() {
-    res.sendStatus(200);
-  });
-});*/
-
-app.get('/get', function(req, res) {
-	db.reference.Asyncfind( { "projectID" : req.params.projectID } );
-
-})
-
 
 app.listen(3000,function(){
     console.log("Working on port 3000");
