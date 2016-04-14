43beb19f124653939594a6133f1b3230977faf51
EsaNuurtamo <esa.nuurtamo@helsinki.fi>
2016-04-14 17:52:24 +0300

    korjaus kontrolleriin

--- controllers/list_and_add_controller.js
+++ controllers/list_and_add_controller.js
@@ -4,15 +4,21 @@ MainApp.controller('MainController', function($scope,$location, $http){
     $scope.newReference={};
     $scope.references=[];
 
-    $http.get("/getAll").success(function(references){
-    	
-    	$scope.references = references;
+    $scope.getReferences=(function(){
+        $http.get("/getAll").success(function(references){
+            $scope.references = references;
+        });
     });
+
+    
     
     $scope.addReference=(function(){
-        $scope.references.push($scope.newReference);
-        $scope.newReference={};
-    	$http.post("/new",$scope.newReference);
+        $http.post("/new",$scope.newReference).success(function(){
+            $scope.newReference={};
+            $scope.getReferences();
+        });
+
+        
     });
         
         
