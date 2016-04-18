83c9610025cd81438eb35e0c410e299bb0c76942
EsaNuurtamo <esa.nuurtamo@helsinki.fi>
2016-04-18 14:43:12 +0300

    started making new tests

--- test/index_test.js
+++ test/index_test.js
@@ -1,11 +1,84 @@
 process.env.NODE_ENV = "test";
-var Browser = require('zombie');
+
 var expect = require('expect.js');
 var app = require("../server.js");
 var http = require("http");
 
 
-describe('When visiting the main page', function(){
+const Browser = require('zombie');
+
+// We're going to make requests to http://example.com/signup
+// Which will be routed to our test server localhost:3000
+Browser.localhost('example.com', 3000);
+
+describe('User visits mainpage', function() {
+
+  browser = new Browser();
+
+  before(function(done) {
+    browser.visit('/', done);
+  });
+
+  describe('loads page', function() {
+
+    it('should be successful', function() {
+      browser.assert.success();
+    });
+
+    it('should see welcome page', function() {
+      browser.assert.text('h1', 'References');
+    });
+  });
+
+  describe('can add valid reference', function() {
+
+  	before(function(done) {
+  		browser
+    	.select('select', 'Article')
+    	.fill('author', 'Matti')
+        .fill('title', 'Minun viite')
+        .pressButton('Add', done);
+
+
+  	});
+	
+	it('form is shown', function() {
+      browser.assert.success();
+    });
+
+    it('should list new reference', function() {
+	      var list=browser.text('a');
+	      var boolean=list.indexOf('Author: Matti, Title: Minun viite')>-1;
+	      expect(boolean).to.be(true);
+    });
+  });
+
+
+  describe('can not add invalid reference', function() {
+	
+	before(function(done) {
+  		browser
+    	.select('select', 'Article')
+    	.fill('author', 'Matti')
+        .fill('title', 'Minun viite')
+        .pressButton('Add', done);
+
+
+  	});
+	
+	it('form is shown', function() {
+      browser.assert.success();
+    });
+
+    it('should list new reference', function() {
+	      var list=browser.text('a');
+	      var boolean=list.indexOf('Author: Matti, Title: Minun viite')>-1;
+	      expect(boolean).to.be(true);
+    });
+  });
+});
+
+/*describe('When visiting the main page', function(){
 	browser = new Browser();
 
 	before(function(done){
@@ -20,7 +93,7 @@ describe('When visiting the main page', function(){
 	});
 
 	it('at least something works', function(done){
-		expect(1==1);
+		browser.assert.text("title","Refernece");
 		done();
 	});
-});
+});*/
