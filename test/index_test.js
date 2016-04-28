4f7b1ed8a538131eb49628c5bba7574bf2d91176
jclc <jopa.93@hotmail.com>
2016-04-28 13:17:44 +0300

    fixed indentation from spaces to tabs

--- test/index_test.js
+++ test/index_test.js
@@ -12,266 +12,266 @@ const Browser = require('zombie');
 Browser.localhost('example.com', 3000);
 
 describe('User visits mainpage', function() {
-  //todo tyhjennä kaikki
-  browser = new Browser();
+	//todo tyhjennä kaikki
+	browser = new Browser();
 
-  before(function(done) {
-    browser.visit('/', done);
-  });
+	before(function(done) {
+		browser.visit('/', done);
+	});
 
-  describe('loads page', function() {
-  
-    it('should be successful', function() {
-      browser.assert.success();
-    });
+	describe('loads page', function() {
+	
+		it('should be successful', function() {
+			browser.assert.success();
+		});
 
-    it('should see welcome page', function() {
-      browser.assert.text('h1', 'References');
-    });
+		it('should see welcome page', function() {
+			browser.assert.text('h1', 'References');
+		});
 
 		//TEST BIBTEX ?? ?? ? ??? ?? ?? ??
 		it('page contains bibtex generating button', function() {
-      browser.assert.attribute('button', 'Generate Bibtext');
+			browser.assert.attribute('button', 'Generate Bibtext');
 		});
-  });
+	});
 
-  describe('loads bibtex', function() {
+	describe('loads bibtex', function() {
 
-    it('should download bibtex', function() {
-      browser.fetch('http://localhost:3000/bib')
-      .then(function(response) {
-      console.log('Status code:', response.status);
-      if (response.status === 200)
-        browser.assert.success();
-      })
-      .then(function(text) {
-        console.log('bibtex testattu');
-      })
-      .catch(function(error) {
-        browser.assert.failure();
-      });
-    });
-  });
+		it('should download bibtex', function() {
+			browser.fetch('http://localhost:3000/bib')
+			.then(function(response) {
+			console.log('Status code:', response.status);
+			if (response.status === 200)
+				browser.assert.success();
+			})
+			.then(function(text) {
+				console.log('bibtex testattu');
+			})
+			.catch(function(error) {
+				browser.assert.failure();
+			});
+		});
+	});
 
 
-  //TEST ARTICLE
-  describe('adds valid Article', function() {
+	//TEST ARTICLE
+	describe('adds valid Article', function() {
 
-    before(function(done) {
-      browser
-      .select('select', 'Article')
-      .fill('authorA', 'Matti')
-      .fill('titleA', 'Minun viite')
-      .fill('journalA', 'Maailmojen Sota II')
-      .fill('yearA', '1917')
-      .pressButton('Add', done);
-    });
+		before(function(done) {
+			browser
+			.select('select', 'Article')
+			.fill('authorA', 'Matti')
+			.fill('titleA', 'Minun viite')
+			.fill('journalA', 'Maailmojen Sota II')
+			.fill('yearA', '1917')
+			.pressButton('Add', done);
+		});
 
-    it('it is added to the listing', function() {
-        var list=browser.text('a');
-        var boolean=list.indexOf('Author: Matti, Title: Minun viite, Type: article')>-1;
-        expect(boolean).to.be(true);
-    });
-  });
+		it('it is added to the listing', function() {
+				var list=browser.text('a');
+				var boolean=list.indexOf('Author: Matti, Title: Minun viite, Type: article')>-1;
+				expect(boolean).to.be(true);
+		});
+	});
 
-  describe('when trying to add an invalid Article', function() {
-  
-  before(function(done) {
-      browser
-      .select('select', 'Article');
-        browser.fill('authorA', 'Matti');
-        done();
-    });
+	describe('when trying to add an invalid Article', function() {
+	
+	before(function(done) {
+			browser
+			.select('select', 'Article');
+				browser.fill('authorA', 'Matti');
+				done();
+		});
 
-    it('the button should be disabled', function() {
-      browser.assert.attribute('#addReference', 'disabled', 'disabled');
-      // var list=browser.text('a');
-      // var boolean=list.indexOf('Author: Matti, Title: , Type: article')>-1;
-      // expect(boolean).to.be(false);
-    });
-  });
+		it('the button should be disabled', function() {
+			browser.assert.attribute('#addReference', 'disabled', 'disabled');
+			// var list=browser.text('a');
+			// var boolean=list.indexOf('Author: Matti, Title: , Type: article')>-1;
+			// expect(boolean).to.be(false);
+		});
+	});
 
  //INPROCEEDING TEST
  describe('can add a valid Inproceeding', function() {
 
-    before(function(done) {
-      browser
-      .select('select', 'Inproceeding')
-        .fill('authorI', 'Mattis')
-        .fill('titleI', 'Minusn viite')
-        .fill('bookTitleI', 'Minun Elämäni VII')
-        .fill('yearI', '9253')
-        .pressButton('Add', done);
-    });
+		before(function(done) {
+			browser
+			.select('select', 'Inproceeding')
+				.fill('authorI', 'Mattis')
+				.fill('titleI', 'Minusn viite')
+				.fill('bookTitleI', 'Minun Elämäni VII')
+				.fill('yearI', '9253')
+				.pressButton('Add', done);
+		});
 
-    it('should list new reference', function() {
-        var list=browser.text('a');
-        var boolean=list.indexOf('Author: Mattis, Title: Minusn viite, Type: inproceeding')>-1;
-        expect(boolean).to.be(true);
-    });
-  });
+		it('should list new reference', function() {
+				var list=browser.text('a');
+				var boolean=list.indexOf('Author: Mattis, Title: Minusn viite, Type: inproceeding')>-1;
+				expect(boolean).to.be(true);
+		});
+	});
 
-  describe('when trying to add an invalid inproceeding', function() {
-  
-  before(function(done) {
-      browser
-      .select('select', 'Inproceeding');
-        browser.fill('pagesI', 'eeeee')
-        .fill('publisherI', 'ooooo')
-        .fill('addressI', 'ooooo');
-        done();
-    });
+	describe('when trying to add an invalid inproceeding', function() {
+	
+	before(function(done) {
+			browser
+			.select('select', 'Inproceeding');
+				browser.fill('pagesI', 'eeeee')
+				.fill('publisherI', 'ooooo')
+				.fill('addressI', 'ooooo');
+				done();
+		});
 
-    it('the button should be disabled', function() {
-      
-      browser.assert.attribute('#addInproceeding', 'disabled', 'disabled');
-        // var list=browser.text('a');
-        // var boolean=list.indexOf('Author: Mattie, Title: , Type: Article')>-1;
-        // expect(boolean).to.be(false);
-    });
-  });
+		it('the button should be disabled', function() {
+			
+			browser.assert.attribute('#addInproceeding', 'disabled', 'disabled');
+				// var list=browser.text('a');
+				// var boolean=list.indexOf('Author: Mattie, Title: , Type: Article')>-1;
+				// expect(boolean).to.be(false);
+		});
+	});
 
-   //BOOK TESTS
-   describe('can add a valid Book with author', function() {
+	 //BOOK TESTS
+	 describe('can add a valid Book with author', function() {
 
-    before(function(done) {
-      browser
-    .select('select', 'Book')
-        .fill('authorB', 'Mattis')
-        .fill('titleB', 'Minsun viites')
-        .fill('publisherB', 'Matti\'s publishser')
-        .fill('yearB', '2')
-        .pressButton('Add', done);
-    });
+		before(function(done) {
+			browser
+		.select('select', 'Book')
+				.fill('authorB', 'Mattis')
+				.fill('titleB', 'Minsun viites')
+				.fill('publisherB', 'Matti\'s publishser')
+				.fill('yearB', '2')
+				.pressButton('Add', done);
+		});
 
-    it('should list new reference', function() {
-        var list=browser.text('a');
-        var boolean=list.indexOf('Author: Mattis, Title: Minsun viites, Type: book')>-1;
-        expect(boolean).to.be(true);
-    });
-  });
+		it('should list new reference', function() {
+				var list=browser.text('a');
+				var boolean=list.indexOf('Author: Mattis, Title: Minsun viites, Type: book')>-1;
+				expect(boolean).to.be(true);
+		});
+	});
 
-  describe('can add a valid Book with editor', function() {
+	describe('can add a valid Book with editor', function() {
 
-    before(function(done) {
-      browser
-    .select('select', 'Book')
-        .fill('editorB', 'Mattiz')
-        .fill('titleB', 'Minzun viites')
-        .fill('publisherB', 'Matti\'z publishser')
-        .fill('yearB', '3')
-        .pressButton('Add', done);
-    });
+		before(function(done) {
+			browser
+		.select('select', 'Book')
+				.fill('editorB', 'Mattiz')
+				.fill('titleB', 'Minzun viites')
+				.fill('publisherB', 'Matti\'z publishser')
+				.fill('yearB', '3')
+				.pressButton('Add', done);
+		});
 
-    it('should list new reference', function() {
-        var list=browser.text('a');
-        var boolean=list.indexOf('Author: Mattis, Title: Minsun viites, Type: book')>-1;
-        expect(boolean).to.be(true);
-    });
-  });
+		it('should list new reference', function() {
+				var list=browser.text('a');
+				var boolean=list.indexOf('Author: Mattis, Title: Minsun viites, Type: book')>-1;
+				expect(boolean).to.be(true);
+		});
+	});
 
-  describe('can not add invalid Book', function() {
-  
-    before(function(done) {
-      browser
-      
-      .select('select', 'Book')
-      .fill('publisherB', 'Mattib')
-      .fill('yearB', 'Manun viiteb');
-      done();
-    });
+	describe('can not add invalid Book', function() {
+	
+		before(function(done) {
+			browser
+			
+			.select('select', 'Book')
+			.fill('publisherB', 'Mattib')
+			.fill('yearB', 'Manun viiteb');
+			done();
+		});
 
-    it('the button should be disabled', function() {
-      
-      browser.assert.attribute('#addBook', 'disabled', 'disabled');
-    });
+		it('the button should be disabled', function() {
+			
+			browser.assert.attribute('#addBook', 'disabled', 'disabled');
+		});
 
 
-  });
-  
-  describe('updating works with correct values', function() {
-  
-    before(function(done) {
-      browser
-        .select('select', 'Book')
-        .fill('authorB', 'Ma')
-        .fill('titleB', 'My')
-        .fill('publisherB', 'Matti\'z publishser')
-        .fill('yearB', '3')
-        .pressButton('Add', done);
-    });
+	});
+	
+	describe('updating works with correct values', function() {
+	
+		before(function(done) {
+			browser
+				.select('select', 'Book')
+				.fill('authorB', 'Ma')
+				.fill('titleB', 'My')
+				.fill('publisherB', 'Matti\'z publishser')
+				.fill('yearB', '3')
+				.pressButton('Add', done);
+		});
 
-    it('clicking link opens the entry', function(done) {
-      browser.clickLink('Author: Ma, Title: My, Type: book', function() {
-        browser.assert.success();
-        browser.assert.text('h1', '"My", type:book')
-        browser.fill('author', 'Sipulisalaatti')
-        browser.pressButton('Update')
-        browser.visit('/', done);
-      });
-    });
+		it('clicking link opens the entry', function(done) {
+			browser.clickLink('Author: Ma, Title: My, Type: book', function() {
+				browser.assert.success();
+				browser.assert.text('h1', '"My", type:book')
+				browser.fill('author', 'Sipulisalaatti')
+				browser.pressButton('Update')
+				browser.visit('/', done);
+			});
+		});
 
-    it('updating changes data', function() {
-      var list=browser.text('a');
-      var boolean=list.indexOf('Author: Sipulisalaatti, Title: My, Type: book')>-1;
-      expect(boolean).to.be(true);
-    });
-  });
+		it('updating changes data', function() {
+			var list=browser.text('a');
+			var boolean=list.indexOf('Author: Sipulisalaatti, Title: My, Type: book')>-1;
+			expect(boolean).to.be(true);
+		});
+	});
 
-  describe('updating does not work with incorrect values', function() {
-  
-    before(function(done) {
-      browser
-        .select('select', 'Book')
-        .fill('authorB', 'Maaa')
-        .fill('titleB', 'Myyy')
-        .fill('publisherB', 'Matti\'z publishser')
-        .fill('yearB', '3')
-        .pressButton('Add', done);
-    });
+	describe('updating does not work with incorrect values', function() {
+	
+		before(function(done) {
+			browser
+				.select('select', 'Book')
+				.fill('authorB', 'Maaa')
+				.fill('titleB', 'Myyy')
+				.fill('publisherB', 'Matti\'z publishser')
+				.fill('yearB', '3')
+				.pressButton('Add', done);
+		});
 
-    it('clicking link opens an entry, update button disabled with incorrect data', function(done) {
-      browser.clickLink('Author: Maaa, Title: Myyy, Type: book', function() {
-        browser.assert.success();
-        browser.assert.text('h1', '"Myyy", type:book')
-        browser.fill('author', '')
-        browser.assert.attribute('#updateReference', 'disabled', 'disabled');
-        browser.visit('/', done);
-      });
-    });
+		it('clicking link opens an entry, update button disabled with incorrect data', function(done) {
+			browser.clickLink('Author: Maaa, Title: Myyy, Type: book', function() {
+				browser.assert.success();
+				browser.assert.text('h1', '"Myyy", type:book')
+				browser.fill('author', '')
+				browser.assert.attribute('#updateReference', 'disabled', 'disabled');
+				browser.visit('/', done);
+			});
+		});
 
-    it('data remains unchanged', function() {
-      var list=browser.text('a');
-      var boolean=list.indexOf('Author: Maaa, Title: Myyy, Type: book')>-1;
-      expect(boolean).to.be(true);
-    });
-  });
+		it('data remains unchanged', function() {
+			var list=browser.text('a');
+			var boolean=list.indexOf('Author: Maaa, Title: Myyy, Type: book')>-1;
+			expect(boolean).to.be(true);
+		});
+	});
 
-  describe('deleting an entry', function() {
-  
-    before(function(done) {
-      browser
-        .select('select', 'Book')
-        .fill('authorB', 'Mb')
-        .fill('titleB', 'Myb')
-        .fill('publisherB', 'Matti\'z publishser')
-        .fill('yearB', '3')
-        .pressButton('Add', done);
-    });
+	describe('deleting an entry', function() {
+	
+		before(function(done) {
+			browser
+				.select('select', 'Book')
+				.fill('authorB', 'Mb')
+				.fill('titleB', 'Myb')
+				.fill('publisherB', 'Matti\'z publishser')
+				.fill('yearB', '3')
+				.pressButton('Add', done);
+		});
 
-    it('clicking link opens an entry, delete button has functionality', function(done) {
-      browser.clickLink('Author: Mb, Title: Myb, Type: book', function() {
-        browser.assert.success();
-        browser.assert.text('h1', '"Myb", type:book')
-        browser.pressButton('#deleteReference')
-        browser.visit('/', done);
-      });
-    });
+		it('clicking link opens an entry, delete button has functionality', function(done) {
+			browser.clickLink('Author: Mb, Title: Myb, Type: book', function() {
+				browser.assert.success();
+				browser.assert.text('h1', '"Myb", type:book')
+				browser.pressButton('#deleteReference')
+				browser.visit('/', done);
+			});
+		});
 
-    it('deleted entry is gone', function() {
-      var list=browser.text('a');
-      var boolean=list.indexOf('Author: Mb, Title: Myb, Type: book')>-1;
-      expect(boolean).to.be(false);
-    });
-  });
+		it('deleted entry is gone', function() {
+			var list=browser.text('a');
+			var boolean=list.indexOf('Author: Mb, Title: Myb, Type: book')>-1;
+			expect(boolean).to.be(false);
+		});
+	});
 });
