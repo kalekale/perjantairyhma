80386632b85198a7998a919803717d648f2f7375
kalekale <lehikoinen.kalle@gmail.com>
2016-04-28 10:11:33 +0300

    testaan mongo herokuu

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 var Promise = require('bluebird');
 var mongojs = require('mongojs');
 
-//var db = mongojs('mongodb://localhost:27017/perjantairyhma', ['references']);
-var db = mongojs('mongodb://kalekale:gooD7soh@ds021731.mlab.com:21731/heroku_r7zfx7t4', ['references']);
+var db = mongojs('mongodb://localhost:27017/perjantairyhma', ['references']);
+//var db = mongojs('mongodb://kalekale:gooD7soh@ds021731.mlab.com:21731/heroku_r7zfx7t4', ['references']);
 
 Promise.promisifyAll(db['references']);
 
