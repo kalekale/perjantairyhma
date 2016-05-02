bac23fd1067d7ea332b5fda488e9c910199a352b
kalekale <lehikoinen.kalle@gmail.com>
2016-05-02 11:31:50 +0300

    bibtex valituista

--- generator.js
+++ generator.js
@@ -27,6 +27,28 @@ function writeFile(cb) {
 	})
 };
 
+function writeFile(allRefs, cb) {
+
+    var stream = fs.createWriteStream("tmp/sources.bibtex");
+    stream.once('open', function(fd) {
+      _.map(allRefs, function(ref) {
+        stream.write('@' + ref.type + '{' + ref._id + ',');
+        stream.write('\n');
+        _.mapValues(ref, function(value, prop) {
+          if (prop == 'type' || prop == '_id') {
+          } else {
+          stream.write(prop + ' = {' + value + '},\n');
+          }
+        })
+        stream.write('}\n\n');
+      })
+      stream.end();
+      stream.on('finish', function() {
+        cb();
+      })
+    });
+};
+
 
 module.exports = {
 	writeFile: writeFile,
